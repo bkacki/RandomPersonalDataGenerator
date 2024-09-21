932f0bac305bb10df88c142abafc7947b966daa1
Bartek <bartlomiej.kacki@gmail.com>
2024-09-21 18:48:28 +0200

    Added birth date generator and PESEL generator.

--- RandomPersonalDataGenerator/Program.cs
+++ RandomPersonalDataGenerator/Program.cs
@@ -1,4 +1,9 @@
-﻿namespace RandomPersonalDataGenerator
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace RandomPersonalDataGenerator
 {
     internal class Program
     {
@@ -6,19 +11,39 @@
         {
             PhoneNumberGenerator phoneNumberGenerator = new PhoneNumberGenerator();
             RandomNameGenerator randomNameGenerator = new RandomNameGenerator();
+            BirthDateGenerator birthDateGenerator = new BirthDateGenerator();
 
             var listOfNumbers = phoneNumberGenerator.GenerateRandomPhoneNumber(100);
             var listOfMaleNames = randomNameGenerator.GenerateRandomNames(Gender.Male, 100);
             var listOfFemaleNames = randomNameGenerator.GenerateRandomNames(Gender.Female, 100);
+            var listOfBirthDates = birthDateGenerator.GenerateRandomBirthDates(100);
 
-            foreach (var number in listOfNumbers)
-                Console.WriteLine(number);
+            RandomPeselGenerator randomPeselGenerator = new RandomPeselGenerator(listOfBirthDates);
+            var listOfPESEL = randomPeselGenerator.GenerateRandomPeselList(Gender.Male, 100);
 
-            foreach(var name in listOfMaleNames)
-                Console.WriteLine(name);
+            for (int i = 0; i < listOfPESEL.Count; i++)
+                Console.WriteLine($"{listOfMaleNames[i],-30}{listOfNumbers[i],-20}{listOfBirthDates[i].ToString("dd-MM-yyyy"),-15}{listOfPESEL[i],-15}");
+        }
 
-            foreach (var name in listOfFemaleNames)
-                Console.WriteLine(name);
+        static async Task RandomAddressAsync()
+        {
+            string apiKey = "YOUR_API_KEY";
+            Random random = new Random();
+            double lat = random.NextDouble() * 180 - 90; // Losowa szerokość geograficzna
+            double lng = random.NextDouble() * 360 - 180; // Losowa długość geograficzna
+
+            string url = $"https://maps.googleapis.com/maps/api/geocode/json?latlng={lat},{lng}&key={apiKey}";
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                JObject json = JObject.Parse(responseBody);
+
+                string address = json["results"]?[0]?["formatted_address"]?.ToString();
+                Console.WriteLine($"Losowy adres: {address}");
+            }
         }
     }
 }
