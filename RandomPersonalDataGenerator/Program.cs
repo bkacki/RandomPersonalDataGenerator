b67770264c7f911c479ccc8702f4e73fe0996815
Bartek <bartlomiej.kacki@gmail.com>
2024-09-22 16:54:23 +0200

    Added random address generator.

--- RandomPersonalDataGenerator/Program.cs
+++ RandomPersonalDataGenerator/Program.cs
@@ -1,49 +1,35 @@
 ﻿using System;
+using System.Globalization;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
 
 namespace RandomPersonalDataGenerator
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             PhoneNumberGenerator phoneNumberGenerator = new PhoneNumberGenerator();
             RandomNameGenerator randomNameGenerator = new RandomNameGenerator();
             BirthDateGenerator birthDateGenerator = new BirthDateGenerator();
+            RandomAddress randomAddress = new RandomAddress();
 
             var listOfNumbers = phoneNumberGenerator.GenerateRandomPhoneNumber(100);
             var listOfMaleNames = randomNameGenerator.GenerateRandomNames(Gender.Male, 100);
             var listOfFemaleNames = randomNameGenerator.GenerateRandomNames(Gender.Female, 100);
-            var listOfBirthDates = birthDateGenerator.GenerateRandomBirthDates(100);
 
+            var listOfBirthDates = birthDateGenerator.GenerateRandomBirthDates(100);
             RandomPeselGenerator randomPeselGenerator = new RandomPeselGenerator(listOfBirthDates);
+
             var listOfPESEL = randomPeselGenerator.GenerateRandomPeselList(Gender.Male, 100);
 
             for (int i = 0; i < listOfPESEL.Count; i++)
-                Console.WriteLine($"{listOfMaleNames[i],-30}{listOfNumbers[i],-20}{listOfBirthDates[i].ToString("dd-MM-yyyy"),-15}{listOfPESEL[i],-15}");
+                Console.WriteLine($"{listOfMaleNames[i],-30}{listOfNumbers[i],-20}{listOfBirthDates[i].ToString("dd-MM-yyyy"),-15}{listOfPESEL[i],-15}{await randomAddress.RandomAddressAsync(),-15}");
         }
 
-        static async Task RandomAddressAsync()
-        {
-            string apiKey = "YOUR_API_KEY";
-            Random random = new Random();
-            double lat = random.NextDouble() * 180 - 90; // Losowa szerokość geograficzna
-            double lng = random.NextDouble() * 360 - 180; // Losowa długość geograficzna
-
-            string url = $"https://maps.googleapis.com/maps/api/geocode/json?latlng={lat},{lng}&key={apiKey}";
-
-            using (HttpClient client = new HttpClient())
-            {
-                HttpResponseMessage response = await client.GetAsync(url);
-                response.EnsureSuccessStatusCode();
-                string responseBody = await response.Content.ReadAsStringAsync();
-                JObject json = JObject.Parse(responseBody);
-
-                string address = json["results"]?[0]?["formatted_address"]?.ToString();
-                Console.WriteLine($"Losowy adres: {address}");
-            }
-        }
+        
     }
 }
